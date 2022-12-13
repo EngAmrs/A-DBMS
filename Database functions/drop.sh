95b3480017461cff5f45731a30c6c1ba8daaccba
Amr <amr.abdoullah@gmail.com>
2022-12-13 20:45:55 +0200

    Phase1

--- Database functions/drop.sh
+++ Database functions/drop.sh
@@ -2,15 +2,17 @@
 
 while true
 do
-if [[ -d $dropName ]]; then
-	rm -R $dropName
+
+dropName=$(dialog --title "Droping..." --inputbox "Enter the database name" 10 60 3>&1 1>&2 2>&3)
+dropName+=.db
+if [[ -d databases/$dropName ]]; then
+	rm -R databases/$dropName
 	dialog --title "Drop Databse" --msgbox "$dropName database has been dropped sucessfully" 9 50
 	#echo "$dropName database has been dropped sucessfully"
 	break
 else
 	dialog --title "Drop Databse" --msgbox "$dropName database not found!" 9 50
 	#echo "Database not found!"
-	DB_menu
 	break
 fi
 done
--- Run.sh
+++ Run.sh
@@ -5,12 +5,18 @@ function welcome(){
 # Welcome message 
 ########################
 clear
+echo ""
+echo ""
 echo " _ _ _       _                       "
 echo "| | | | ___ | | ___  ___ ._ _ _  ___ "
 echo "| | | |/ ._>| |/ | '/ . \| ' ' |/ ._>"
 echo "|__/_/ \___.|_|\_|_.\___/|_|_|_|\___."
 echo "                                     "
-
+echo "		 ___      "
+echo "		|_ _|___  "
+echo "		 | |/ . \ "
+echo "		 |_|\___/ "
+echo "          "
 echo "		 _ _ ___  _ _  ___  ___  ___  ___  ___  ___  ___  ___ "
 echo "		|/|/| . ||/|/ | . \| . ||_ _|| . || . >| . |/ __>| __>"
 echo "		    |   |     | | ||   | | | |   || . \|   |\__ \| _> "
@@ -37,29 +43,27 @@ DB_Menu=$(dialog --title "DBMS Menu" --fb --menu "Select: " 12 70 4 \
 		1)
 			echo "Create DataBase"
 			. ./"Database functions"/createDB.sh
-		        DB_menu
+		    DB_menu
 			;;	
 		2)
             echo "List & Number of  DataBases"
 			cd databases
 			databaselist=$(ls -d *.db)
-            		databaseNo=$(ls -d *.db | cut -f1 -d"/" | wc -w)
+            databaseNo=$(ls -d *.db | cut -f1 -d"/" | wc -w)
 			dialog --title "Number of DataBases  No-{$databaseNo}" --msgbox "$databaselist" 20 45
 			cd ../
 			DB_menu
             ;;
 		3)
-				echo "Connect to Database"
-				dbConnect=$(dialog --title "Conecting..." --inputbox "Enter the database name" 10 60 3>&1 1>&2 2>&3)
-				echo $dbConnect
-				. ./"Database functions"/connect.sh	
+			echo "Connect to Database"
+			. ./"Database functions"/connect.sh
+			DB_menu
 			;;
 		4)
 			echo "Drop Database"
-			dropName=$(dialog --title "Droping..." --inputbox "Enter the database name" 10 60 3>&1 1>&2 2>&3)
-			echo $dropName
 			. ./"Database functions"/drop.sh
-				;;
+			DB_menu
+			;;
 
 		esac
 clear
